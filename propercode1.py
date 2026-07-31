1a966bcb5cfffabf921db6a848e47353fd508b66
NiskaXD <2630703@iub.edu.bd>
2026-07-31 19:09:38 +0600

    Add files via upload
    2nd code practice

---
+++ propercode1.py
@@ -0,0 +1,95 @@
+# a=int(input("Enter a number1:"))
+# b=int(input("Enter a number2:"))
+# if (a>b):
+#     print("Fuck")
+# else:
+#     print("you")
+
+# def sum(a,b):
+#     sum=a+b
+#     print(sum)
+
+# def check(a):
+#     if a>0:
+#         print("positive")
+#     elif a<0:
+#         print("negative")
+#     else:
+#         print("zero")
+
+# num=int(input("Enter a number:"))
+# check(num)
+
+# def calc(a,b,s):
+#     if s=='+':
+#         print(f'{a}+{b}={a+b}')
+#     elif s=='-':
+#         print(f'{a}-{b}={a-b}')
+#     elif s=='*':
+#         print(f'{a}X{b}={a*b}')
+#     elif s=='/':
+#         print(f'{a}/{b}={a/b:.3f}')
+#     elif s=='//':
+#         print(f'{a}//{b}={a//b}')
+#     elif s=='%':
+#         print(f'{a}%{b}={a%b}')
+
+# a=int(input("enter a number:"))
+# b=int(input("enter another number:"))
+# s=input("Enter an arithmetic operator:")
+# calc(a,b,s)
+
+
+
+
+#salary = float(input("Enter the employee's current salary: "))
+
+
+# if salary <=400: #0-400.00
+#     rate = 0.15  #15%=15/100=0.15
+# elif salary <= 800: #400.01-800.00
+#     rate = 0.12
+# elif salary <= 1200:#800.01-1200.00
+#     rate = 0.10
+# elif salary <= 2000:#1200.01-2000.00
+#     rate = 0.07  #7%=7/100=0/07
+# else: #above 2000.00
+#     rate = 0.04
+# increase_salary = salary * rate #Example: 400*0.15=60
+# new_salary = salary + increase_salary #Example: 400+60=460
+
+
+# print(f"Previous Salary: {salary:.0f}")
+# print(f"New salary: {new_salary:.0f}")
+# print(f"Increase percentage: {rate * 100:.0f}%")
+
+
+# def calc(a,b,s):
+#     if s=='+':
+#         print(f'{a}+{b}={a+b}')
+#     elif s=='-':
+#         print(f'{a}-{b}={a-b}')
+#     elif s=='*':
+#         print(f'{a}X{b}={a*b}')
+#     elif s=='/':
+#         print(f'{a}/{b}={a/b:.3f}')
+#     elif s=='//':
+#         print(f'{a}//{b}={a//b}')
+#     elif s=='%':
+#         print(f'{a}%{b}={a%b}')
+
+
+
+''' well lets see if it gets printed.'''
+# print("hello world")
+#print("hey im \"good\" boy")
+# for num in range (10):
+#     print(num,end="")
+# for num in range (10):
+#     print(num,end="")
+# for num in range (10):
+# #     print(num,end="")
+# print('hi im"good" ')
+
+# com="i am 7 years old.i need a jOb."
+# print(com.capitalize())
